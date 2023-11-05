967e01f9897493ac2d161a09f8f554c55998ff9f
marcosayres <marcosayres.brasil@gmail.com>
2023-11-05 00:57:05 -0300

    Nova Logomarca

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Defina o caminho completo para a imagem que você deseja usar como papel de parede
-var imagePath = "./assets/kalai.jpeg";
+var imagePath = "./assets/banner_1.png";
 
 // Use o seguinte código para definir a imagem como papel de parede
 document.body.style.backgroundImage = "url('" + imagePath + "')";
-document.body.style.backgroundSize = "cover";
+document.body.style.backgroundSize = "100% 800px";
 document.body.style.backgroundRepeat = "no-repeat";
